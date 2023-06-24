1a81c04867b474d4e3a6151316c53e46e29f5c20
Kherbache Yacine <38230283+Yaci016@users.noreply.github.com>
2023-06-24 14:52:36 +0200

    fix commands to launch app on production (#63)
    Co-authored-by: Yacine Kherbache <yacineKherbache@yaci.fr>

--- back/app/config/app.config.js
+++ back/app/config/app.config.js
@@ -8,8 +8,14 @@ const __dirname = dirname(__filename);
 
 loadEnv(); // load environment variables
 
+
+//setup origin array (accepted url that can hit the endpoints of the api)
+const origin = ["http://localhost:3000"];
+if (process.env.CORS_ORIGIN) {
+    origin.push(process.env.CORS_ORIGIN);
+}
 export const config = {
   port: process.env.PORT || 8080,
-  corsOptions: { origin: "http://localhost:3000" },
+  corsOptions: { origin: origin },
   uploads: path.join(__dirname, "/uploads"),
 };
