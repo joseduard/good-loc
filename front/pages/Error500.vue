53669c2024a9037a0450fd314a73d64ad1adc977
Jonathan Piboteau <87586954+JoePibs@users.noreply.github.com>
2023-07-05 21:10:09 +0200

    change Html & css on error Vue (#91)

--- front/pages/Error500.vue
+++ front/pages/Error500.vue
@@ -1,26 +1,22 @@
 <template>
   <v-container id="nuxt-error" fluid>
+    <h1 class="display-1 fw-400">Oops une erreur s'est produite</h1>
     <v-row>
-      <v-col cols="12" md="12" lg="12">
-        <h1 class="fw-400 primary--text">Contacter l'administrateur</h1>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="d-flex justify-center" cols="12" md="12" lg="12">
+      <v-col cols="12" md="6" lg="6">
         <img class="img-error" src="../assets/404.png" alt="" />
       </v-col>
-    </v-row>
-    <!-- <v-row>
-      <v-col  class="d-flex justify-center"  cols="12" md="12" lg="12">
-        <p class="hint">
-          <i class="fw-400 primary--text">Semble case</i>
+      <v-col class="d-flex justify-center" cols="12" md="6" lg="6"> 
+        <p class="quote">
+          <span class="quote-text">"Même les licornes les plus pures se trompent des fois de chemin"</span>
+          <br />
+          <span class="quote-author">Licorne Kenobi - Licornus Maxima Book- verset 3</span>
         </p>
       </v-col>
-    </v-row> -->
+    </v-row>
     <v-row>
       <v-col class="d-flex justify-center" cols="12" md="12" lg="12">
         <p class="hint">
-          <v-btn text tile to="/">
+          <v-btn class="back" to="/">
             <h3 class="fw-400 primary--text">revenir à l'accueil</h3>
           </v-btn>
         </p>
@@ -53,3 +49,4 @@ export default {
   },
 }
 </script>
+
